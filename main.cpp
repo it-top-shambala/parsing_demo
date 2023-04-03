669437f736fc5db81d3327c9358a8513a3a19f0b
elena <haleeva-elena@mail.ru>
2023-04-03 20:44:11 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Превратить строку 03.04.2023 в три числа
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter text : ";
+    getline(cin, input);
+
+
+
+    return 0;
+}
