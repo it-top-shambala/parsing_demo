b2f10cd3de388ad91e80e08c96911d138abc740c
elena <haleeva-elena@mail.ru>
2023-04-03 20:50:38 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -7,10 +7,20 @@ using namespace std;
 
 int main() {
     string input;
-    cout << "Enter text : ";
+    cout << "Enter date (dd.mm.yyyy) : ";
     getline(cin, input);
 
+    // 03.04.2023
+    int position = input.find('.');
+    string _day = input.substr(0, position);
+    input = input.substr(position + 1); // 04.2023
+    position = input.find('.');
+    string _month = input.substr(0, position);
+    string _year = input.substr(position + 1);
 
+    int day = stoi(_day);
+    int month = stoi(_month);
+    int year = stoi(_year);
 
     return 0;
 }
